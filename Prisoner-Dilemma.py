73aa13e48da62b17289f90f93100b48355b4603d
Marty Chang <Ngredmann1013@lsr7.net>
2015-11-12 11:50:14 -0600

    changed date again

--- Prisoner-Dilemma.py
+++ Prisoner-Dilemma.py
@@ -13,10 +13,10 @@ Players should each code their strategies in their assigned section of code.
 
 Aggregated results are stored in tournament.txt
 
-Unpublished work (c)2013 Project Lead The Way 11/12/2015
+Unpublished work (c)2013 Project Lead The Way
 CSE Project 1.3.5 Collaborating on a Project
 Draft, Do Not Distribute
-Version 8/23/2013 
+Version 11/12/2015
 '''
 
 import random
